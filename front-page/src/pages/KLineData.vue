46974df2b8ba2d3e7bed2c0b6706124d5c73de75
dennislee928 <55692615+dennislee928@users.noreply.github.com>
2024-09-03 21:37:11 +0800

    Update KLineData.vue

--- front-page/src/pages/KLineData.vue
+++ front-page/src/pages/KLineData.vue
@@ -1,42 +1,73 @@
 <template>
-  <NavBar />
-  <div>
-    <h1>K Line Data</h1>
-    <div>
-      <label for="market">Market:</label>
-      <select v-model="market" id="market">
-        <option v-for="option in marketOptions" :key="option" :value="option">{{ option }}</option>
-      </select>
+  <div
+    class="min-h-screen bg-gradient-to-r from-purple-600 to-blue-600 text-white flex flex-col items-center"
+  >
+    <h1 class="text-5xl font-extrabold mt-10">K Line Data</h1>
+    <div class="w-full max-w-4xl mt-10 p-6 bg-gray-900 bg-opacity-75 rounded-lg shadow-lg">
+      <div class="mb-4">
+        <label for="market" class="block text-lg">Market:</label>
+        <select v-model="market" id="market" class="w-full p-2 bg-gray-800 text-white rounded">
+          <option v-for="option in marketOptions" :key="option" :value="option">
+            {{ option }}
+          </option>
+        </select>
+      </div>
+      <div class="mb-4">
+        <label for="interval" class="block text-lg">Interval:</label>
+        <select v-model="interval" id="interval" class="w-full p-2 bg-gray-800 text-white rounded">
+          <option v-for="option in intervalOptions" :key="option" :value="option">
+            {{ option }}
+          </option>
+        </select>
+      </div>
+      <div class="mb-4">
+        <label for="startTime" class="block text-lg">Start Time:</label>
+        <div class="block">
+          <el-date-picker
+            v-model="startTime"
+            type="datetime"
+            placeholder="Select date and time"
+            :shortcuts="shortcuts"
+            class="w-full"
+          />
+        </div>
+      </div>
+      <div class="mb-4">
+        <label for="endTime" class="block text-lg">End Time:</label>
+        <div class="block">
+          <el-date-picker
+            v-model="endTime"
+            type="datetime"
+            placeholder="Select date and time"
+            :shortcuts="shortcuts"
+            class="w-full"
+          />
+        </div>
+      </div>
+      <button
+        @click="fetchData"
+        class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-300"
+      >
+        Fetch Data
+      </button>
+      <div v-if="data" class="mt-10 w-full">
+        <pre class="bg-gray-800 p-4 rounded">{{ formattedData }}</pre>
+        <canvas id="klineChart" class="mt-4"></canvas>
+      </div>
     </div>
-    <div>
-      <label for="interval">Interval:</label>
-      <select v-model="interval" id="interval">
-        <option v-for="option in intervalOptions" :key="option" :value="option">
-          {{ option }}
-        </option>
-      </select>
-    </div>
-    <div>
-      <label for="startTime">Start Time:</label>
-      <input type="datetime-local" v-model="startTime" id="startTime" />
-    </div>
-    <div>
-      <label for="endTime">End Time:</label>
-      <input type="datetime-local" v-model="endTime" id="endTime" />
-    </div>
-    <button @click="fetchData">Fetch Data</button>
-    <div v-if="data">
-      <pre>{{ formattedData }}</pre>
-      <canvas id="klineChart"></canvas>
-    </div>
+    <div v-if="!data" class="mt-10 text-xl">Loading...</div>
   </div>
 </template>
 
 <script>
 import { Chart } from 'chart.js'
+import { ElDatePicker } from 'element-plus'
 
 export default {
   name: 'KLineData',
+  components: {
+    ElDatePicker
+  },
   data() {
     return {
       data: null,
@@ -48,78 +79,38 @@ export default {
         'btctwd',
         'ethtwd',
         'ltctwd',
-        'bchtwd',
-        'usdttwd',
-        'ethbtc',
-        'btcusdt',
-        'ethusdt',
-        'bchusdt',
-        'ltcusdt',
-        'xrptwd',
-        'xrpusdt',
-        'maxusdt',
-        'maxtwd',
-        'bcnttwd',
-        'bcntusdt',
-        'usdctwd',
-        'linktwd',
-        'comptwd',
-        'sandusdt',
-        'usdcusdt',
-        'linkusdt',
-        'grttwd',
-        'grtusdt',
-        'yfitwd',
-        'yfiusdt',
-        'dogetwd',
-        'dogeusdt',
-        'adatwd',
-        'dottwd',
-        'matictwd',
-        'compusdt',
-        'dotusdt',
-        'maticusdt',
-        'adausdt',
-        'soltwd',
-        'solusdt',
-        'shibtwd',
-        'shibusdt',
-        'sandtwd',
-        'galatwd',
-        'galausdt',
-        'manatwd',
-        'manausdt',
-        'alicetwd',
-        'aliceusdt',
-        'lookstwd',
-        'rlytwd',
-        'loottwd',
-        'masktwd',
-        'lootusdt',
-        'maskusdt',
-        'apetwd',
-        'apeusdt',
-        'rlyusdt',
-        'xtztwd',
-        'xtzusdt',
-        'gmttwd',
-        'gmtusdt',
-        'gsttwd',
-        'gstusdt',
-        'bnbtwd',
-        'bnbusdt',
-        'enstwd',
-        'ensusdt',
-        'etctwd',
-        'etcusdt',
-        'arbtwd',
-        'arbusdt',
-        'tontwd',
-        'tonusdt',
-        'avaxtwd',
-        'avaxusdt'
+        'xrptwd' // 添加更多市場選項
       ],
-      intervalOptions: [1, 5, 15, 30, 60, 120, 240, 360, 720, 1440, 4320, 10080]
+      intervalOptions: [1, 5, 15, 30, 60, 120, 240, 360, 720, 1440, 4320, 10080],
+      shortcuts: [
+        {
+          text: 'Last 1 hour',
+          value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000)
+            return [start, end]
+          }
+        },
+        {
+          text: 'Last 24 hours',
+          value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24)
+            return [start, end]
+          }
+        },
+        {
+          text: 'Last 7 days',
+          value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            return [start, end]
+          }
+        }
+      ]
     }
   },
   computed: {
@@ -133,11 +124,11 @@ export default {
         const startTime = Math.floor(new Date(this.startTime).getTime() / 1000)
         const endTime = Math.floor(new Date(this.endTime).getTime() / 1000)
 
-        const url = new URL('/api/v3/k', window.location.origin)
-        url.searchParams.append('market', this.market)
-        url.searchParams.append('interval', this.interval)
-        url.searchParams.append('start_time', startTime)
-        url.searchParams.append('end_time', endTime)
+        const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'https://max-api.maicoin.com'
+        const url = new URL(
+          `/api/v3/k?market=${this.market}&interval=${this.interval}&start_time=${startTime}&end_time=${endTime}`,
+          apiBaseUrl
+        )
 
         const response = await fetch(url)
         if (!response.ok) {
@@ -190,10 +181,5 @@ export default {
 </script>
 
 <style scoped>
-pre {
-  background-color: #f8f8f8;
-  padding: 10px;
-  border-radius: 5px;
-  overflow-x: auto;
-}
+/* Tailwind CSS will handle most of the styling */
 </style>
